51457a70b4c23b6337441fda7bf025fe1264f459
Andy West <andy.west@focusrite.com>
2024-07-08 21:26:31 +0100

    added button lockout states

--- src/modules/chordContentProcessor.js
+++ src/modules/chordContentProcessor.js
@@ -2,148 +2,186 @@ const fs = require('fs');
 const path = require('path');
 const { markChords, transposeChords, getChordsWithIndex, getChords } = require('./chordOperations');
 
-function isWhitespaceLine(line) {
-  return /^\s*$/.test(line);
-}
+let chordsMarked = false;
+let chordsNested = false;
 
 // Function to format content with chord highlighting
 function markChordsInContent(content)
 {
-  let outputText = [];
-  const lines = content.split('\n');
-  for (const line of lines) {
-    outputText.push(markChords(line, '{', '}'));
+  result = content;
+  if(!chordsMarked && !chordsNested)
+  {
+    chordsMarked = true;
+    let outputText = [];
+    const lines = content.split('\n');
+    for (const line of lines) {
+      outputText.push(markChords(line, '{', '}'));
+    }
+    result = outputText.join('\n');
   }
-  return outputText.join('\n');
+  return result;
 }
 
 function unMarkChordsInContent(content)
 {
-  // Replace all '{' and '}' with an empty string
-  return content.replace(/\{/g, '').replace(/\}/g, '');
+  result = content;
+  if(chordsMarked && !chordsNested)
+  {
+    chordsMarked = false;
+    result = content.replace(/\{/g, '').replace(/\}/g, '');
+  }
+  return result;
 }
 
-function nestChordsInContent(content) {
-  let lines = content.split('\n');
-  let result = [];
-  if((lines.length %2)!=0){lines.push("");}
-  let n = 0;
-  while(n < lines.length) {
-    let [chords, indexes] = getChordsWithIndex(lines[n]);
-    if((chords.length > 0) && ((n+1) < lines.length) && (getChords(lines[n+1]) == 0) && !lines[n+1].match(/^\s*$/))
-    {
-      let offset = 0;
-      let newLine = lines[n+1]
-      for(let i=0; i<chords.length; i++)
+function nestChordsInContent(content)
+{
+  result = content;
+  if(!chordsNested)
+  {
+    if(chordsMarked){content = unMarkChordsInContent(content);}
+    chordsNested = true;
+    let lines = content.split('\n');
+    let result_lines = [];
+    if((lines.length %2)!=0){lines.push("");}
+    let n = 0;
+    while(n < lines.length) {
+      let [chords, indexes] = getChordsWithIndex(lines[n]);
+      if((chords.length > 0) && ((n+1) < lines.length) && (getChords(lines[n+1]) == 0) && !lines[n+1].match(/^\s*$/))
       {
-        let new_pos = indexes[i] + offset;
-        newLine = (newLine.slice(0, indexes[i] + offset) + '{' + chords[i] + '}' + newLine.slice(new_pos));
-        offset += chords[i].length+2;
+        let offset = 0;
+        let newLine = lines[n+1]
+        for(let i=0; i<chords.length; i++)
+        {
+          let new_pos = indexes[i] + offset;
+          newLine = (newLine.slice(0, indexes[i] + offset) + '{' + chords[i] + '}' + newLine.slice(new_pos));
+          offset += chords[i].length+2;
+        }
+        result_lines.push(newLine);
+        n++;
       }
-      result.push(newLine);
+      else
+      {
+        result_lines.push(lines[n]);
+      }
       n++;
     }
-    else
-    {
-      result.push(lines[n]);
-    }
-    n++;
+    result = result_lines.join('\n');
   }
-  return result.join('\n');
+  return result;
 }
 
 
-function unNestChordsInContent(content) {
-  let lines = content.split('\n');
-  let result = [];
+function unNestChordsInContent(content) 
+{
+  result = content;
+  if(chordsNested)
+  {
+    chordsNested = false;
+    let lines = content.split('\n');
+    let result_lines = [];
 
-  for (let n = 0; n < lines.length; n++) {
-    let line = lines[n];
-    let chords = [];
-    let indices = [];
-    let newLine = '';
+    for (let n = 0; n < lines.length; n++) {
+      let line = lines[n];
+      let chords = [];
+      let indices = [];
+      let newLine = '';
 
-    // Extract chords and their indices
-    line.replace(/\{(.*?)\}/g, (match, p1, offset) => {
-      chords.push(p1);
-      indices.push(offset);
-    });
+      // Extract chords and their indices
+      line.replace(/\{(.*?)\}/g, (match, p1, offset) => {
+        chords.push(p1);
+        indices.push(offset);
+      });
 
-    console.log(chords);
-    console.log(indices);
-
-    // Create a line with chords in their original positions
-    if (chords.length > 0) {
-      let offset = 0;
-      let chordLine = '';
-      for(let i=0; i<chords.length; i++)
-      {
-        indices[i] = indices[i] - offset;
-        offset += chords[i].length + 2; //Bracket LR
-      }
-      let charCount=0;
-      let chordCount=0;
-      for(let i=0; i < indices.length; i++)
-      {
-        let charCountOnEntry = charCount;
-        while(charCount < indices[i])
+      // Create a line with chords in their original positions
+      if (chords.length > 0) {
+        let offset = 0;
+        let chordLine = '';
+        for(let i=0; i<chords.length; i++)
         {
-          chordLine += ' ';
-          charCount++;
+          indices[i] = indices[i] - offset;
+          offset += chords[i].length + 2; //Bracket LR
         }
-        if((indices[i] != 0) && ((charCount - charCountOnEntry) == 0))
+        let charCount=0;
+        let chordCount=0;
+        for(let i=0; i < indices.length; i++)
         {
-          chordLine += ' ';
-          charCount++;
+          let charCountOnEntry = charCount;
+          while(charCount < indices[i])
+          {
+            chordLine += ' ';
+            charCount++;
+          }
+          if((indices[i] != 0) && ((charCount - charCountOnEntry) == 0))
+          {
+            chordLine += ' ';
+            charCount++;
+          }
+          charCount += chords[chordCount].length;
+          chordLine += chords[chordCount++];
         }
-        charCount += chords[chordCount].length;
-        chordLine += chords[chordCount++];
+        result_lines.push(chordLine);
       }
-      result.push(chordLine);
+
+      // Remove chord markers from the current line
+      newLine = line.replace(/\{.*?\}/g, '');
+      result_lines.push(newLine);
     }
 
-    // Remove chord markers from the current line
-    newLine = line.replace(/\{.*?\}/g, '');
-    result.push(newLine);
+    result = result_lines.join('\n');
   }
-
-  return result.join('\n');
+  return result;
 }
 
 function transposeInContent(content, steps)
 {
-  let outputText = [];
-  const lines = content.split('\n');
-  for (const line of lines) {
-    outputText.push(transposeChords(line, steps));
+  result = content;
+  if(!chordsNested)
+  {
+    let reMark = false;
+    if(chordsMarked){content = unMarkChordsInContent(content); reMark = true;}
+    let outputText = [];
+    const lines = content.split('\n');
+    for (const line of lines) {
+      outputText.push(transposeChords(line, steps));
+    }
+    result = outputText.join('\n');
+    if(reMark){result = markChordsInContent(result);}
   }
-  return outputText.join('\n');
+  return result;
 }
 
-function removeBlankLinesInContent(content) {
+function removeBlankLinesInContent(content)
+{
+  result = content;
+  if(!chordsMarked && !chordsNested)
+  {
     // Split the lyrics into lines
     let lines = content.split('\n');
 
-    let result = [];
+    let result_lines = [];
     let n=0;
     while (n < lines.length) {
       let line_n_match = lines[n].match(/^\s*$/);
       if(line_n_match && ((n + 1) < lines.length) && lines[n+1].match(/^\s*$/))
       {
-        result.push("");
+        result_lines.push("");
         n++;
       }
       else if(!line_n_match)
       {
-        result.push(lines[n]);
+        result_lines.push(lines[n]);
       }
       n++
     }
-
     // Join the lines into a single string with newlines and return the result
-    return result.join('\n');
+    result = result_lines.join('\n');
+  }
+  return result;
 }
+
 function loadExample(editor) {
+  chordsMarked = false;
+  chordsNested = false;
   const exampleFilePath = path.join(__dirname, 'example.txt');
   fs.readFile(exampleFilePath, 'utf8', (err, data) => {
     if (err) {
